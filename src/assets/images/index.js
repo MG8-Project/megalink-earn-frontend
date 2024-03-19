3d9164185a059bd2c45957bbe1788d36ab7f3c9c
yeonsu <rhdustn@imantisco.com>
2024-03-19 16:48:06 +0900

    [MG8-84] images upload

--- src/assets/images/index.js
+++ src/assets/images/index.js
@@ -11,3 +11,13 @@ export { default as footerLogo } from "./Megalink_CI.png";
 export { default as binance } from "./binance.png";
 export { default as wemix } from "./wemix.png";
 export { default as ace } from "./ace.png";
+
+// Daily
+export { default as mega8 } from "./mg8.png";
+export { default as mg8gray } from "./mg8gray.png";
+
+// Spin
+export { default as gift } from "./gift.png";
+export { default as ticket } from "./ticket.png";
+export { default as clock } from "./clock.png";
+export { default as spinFrame } from "./Frame_1707484677.png";
