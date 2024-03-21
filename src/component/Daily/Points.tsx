b71428701360031955be2272876e2b9d45622384
augusstt06 <augusstt06@gmail.com>
2024-03-22 01:13:35 +0900

    fix : join user

--- src/component/Daily/Points.tsx
+++ src/component/Daily/Points.tsx
@@ -1,87 +1,85 @@
-import styled, {keyframes} from "styled-components";
-import {useWallet} from "../../hooks/useWallet";
-import {useAuthStore} from "../../store/authStore";
+import styled, { keyframes } from "styled-components";
+import { useWallet } from "../../hooks/useWallet";
+import { useAuthStore } from "../../store/authStore";
 import ApiPoints from "../../apis/ApiPoints";
-import {useState} from "react";
+import { useState } from "react";
 import JoinModal from "../Modal";
 
 const Points = () => {
-    const {connectWallet} = useWallet();
-    const isLoggedIn = useAuthStore((state) => state.isLoggedIn);
-    const walletAddress = useAuthStore((state) => state.userAccount);
-    const userId = useAuthStore((state) => state.userId);
-    const [loginAttemptFailed, setLoginAttemptFailed] = useState(false);
-    const [isModalOpen, setIsModalOpen] = useState(false)
+  const { connectWallet } = useWallet();
+  const isLoggedIn = useAuthStore((state) => state.isLoggedIn);
+  const walletAddress = useAuthStore((state) => state.userAccount);
+  const userId = useAuthStore((state) => state.userId);
+  const [loginAttemptFailed, setLoginAttemptFailed] = useState(false);
+  const [isModalOpen, setIsModalOpen] = useState(false);
 
-    const clickLogin = async () => {
-        try {
-            throw new Error("Not implemented");
-            setLoginAttemptFailed(false);
-            let address: string = null;
-            if (!walletAddress) {
-                address = await connectWallet();
-                if (address === null) {
-                    alert("Please install Metamask and connect your wallet.");
-                    return;
-                }
-                useAuthStore.getState().setUserAccount(address);
-            }
-            let getUserId: string = null;
-            if (!userId) {
-                getUserId = await ApiPoints.getUserId(address);
-            }
-            address = address || walletAddress;
-            getUserId = userId || getUserId;
-
-            await ApiPoints.login(address);
-            useAuthStore.getState().login(address, getUserId);
-            alert("Successfully logged in.");
-        } catch (error) {
-            // FIXME: 여기서 회원가입 모달
-            setIsModalOpen(true);
+  const clickLogin = async () => {
+    try {
+      setLoginAttemptFailed(false);
+      let address: string = null;
+      if (!walletAddress) {
+        address = await connectWallet();
+        if (address === null) {
+          alert("Please install Metamask and connect your wallet.");
+          return;
         }
-    };
-    return (
-        <PointsWrapper>
-            <TextWrapper>
-                <div>My Total MG8 Points</div>
-                <PointText>-P</PointText>
-            </TextWrapper>
-            {isModalOpen ? <JoinModal/> : <> </>}
-            {(!isLoggedIn || loginAttemptFailed) && (
-                <LoginButton onClick={clickLogin}>Login</LoginButton>
-            )}
-        </PointsWrapper>
-    );
+        useAuthStore.getState().setUserAccount(address);
+      }
+      let getUserId: string = null;
+      if (!userId) {
+        getUserId = await ApiPoints.getUserId(address);
+      }
+      address = address || walletAddress;
+      getUserId = userId || getUserId;
+
+      await ApiPoints.login(address);
+      useAuthStore.getState().login(address, getUserId);
+      alert("Successfully logged in.");
+    } catch (error) {
+      setIsModalOpen(true);
+    }
+  };
+  return (
+    <PointsWrapper>
+      <TextWrapper>
+        <div>My Total MG8 Points</div>
+        <PointText>-P</PointText>
+      </TextWrapper>
+      {(!isLoggedIn || loginAttemptFailed) && (
+        <LoginButton onClick={clickLogin}>Login</LoginButton>
+      )}
+      {isModalOpen ? <JoinModal /> : <> </>}
+    </PointsWrapper>
+  );
 };
 export default Points;
 
 const PointsWrapper = styled.div`
-    display: flex;
-    flex-direction: column;
-    align-items: center;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
 `;
 
 const TextWrapper = styled.div`
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    gap: 12px;
-    font-weight: 600;
-    font-size: 18px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  gap: 12px;
+  font-weight: 600;
+  font-size: 18px;
 `;
 const PointText = styled.div`
-    font-size: 40px;
+  font-size: 40px;
 `;
 
 const LoginButton = styled.button`
-    margin-top: 36px;
-    font-weight: 600;
-    width: 120px;
-    height: 56px;
-    border: 1px solid #ffffff;
-    border-radius: 100px;
-    font-size: 20px;
+  margin-top: 36px;
+  font-weight: 600;
+  width: 120px;
+  height: 56px;
+  border: 1px solid #ffffff;
+  border-radius: 100px;
+  font-size: 20px;
 `;
 
 const fadeIn = keyframes`
@@ -94,15 +92,15 @@ const fadeIn = keyframes`
 `;
 
 const JoinModalWrapper = styled.div`
-    position: fixed;
-    top: 0;
-    left: 0;
-    width: 100vw;
-    height: 100vh;
-    background-color: rgba(0, 0, 0, 0.7);
-    z-index: 1000;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    animation: ${fadeIn} 0.3s ease-in-out forwards;
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100vw;
+  height: 100vh;
+  background-color: rgba(0, 0, 0, 0.7);
+  z-index: 1000;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  animation: ${fadeIn} 0.3s ease-in-out forwards;
 `;
--- src/component/Modal/index.tsx
+++ src/component/Modal/index.tsx
@@ -1,125 +1,164 @@
-import React, { useState } from 'react';
-import styled from 'styled-components';
-import { useForm } from 'react-hook-form';
+import React, { ChangeEvent, useState } from "react";
+import styled from "styled-components";
+import { useForm } from "react-hook-form";
+import API from "../../apis/Api";
 
 const JoinModal: React.FC = () => {
-
-    const { register, handleSubmit } = useForm();
-    const [isSubmitting, setIsSubmitting] = useState(false);
-    const onSubmit = async (data: any) => {
-
-        setIsSubmitting(true);
-        // TODO: 회원가입 API 호출
-
-        setIsSubmitting(false);
-
-    };
-    return (
-        <JoinModalWrapper>
-            <JoinModalContent>
-                <JoinModalTitle>Join us</JoinModalTitle>
-                <form onSubmit={handleSubmit(onSubmit)}>
-                    <JoinModalInput
-                        name="name"
-                        placeholder="name"
-                    />
-                    <JoinModalInput
-                        name="country"
-                        placeholder="country"
-                    />
-                    <JoinModalAddress>
-                        {window.ethereum?.selectedAddress}
-                    </JoinModalAddress>
-                    <JoinModalButton type="submit" disabled={isSubmitting}>
-                        {isSubmitting ? '로딩...' : '회원가입'}
-                    </JoinModalButton>
-                </form>
-            </JoinModalContent>
-        </JoinModalWrapper>
-    );
-
+  const { handleSubmit } = useForm();
+  const endpointUser = process.env.REACT_APP_API_USER;
+  const userAccount = window.ethereum?.selectedAddress;
+  const [isSubmitting, setIsSubmitting] = useState(false);
+  const [userName, setUserName] = useState("");
+  const [userNation, setUserNation] = useState(0);
+  const resetInputFields = () => {
+    setUserName("");
+    setUserNation(0);
+  };
+  const fetchJoin = async () => {
+    try {
+      const res = await API.post(
+        `${endpointUser}/join`,
+        {
+          userAccount: userAccount,
+          userName: userName,
+          userNation: userNation,
+        },
+        {
+          headers: {
+            "Content-Type": "application/json",
+          },
+        }
+      );
+      if (res.data.resultCode !== "1") throw new Error(res.data.resultCode);
+      alert("회원가입이 완료되었습니다.");
+      resetInputFields();
+    } catch (err) {
+      switch (err) {
+        case "2":
+          alert("이미 가입된 사용자 정보입니다.");
+          return;
+        default:
+          alert("가입에 실패했습니다.");
+          return;
+      }
+    }
+  };
+  const onSubmit = async (data: any) => {
+    setIsSubmitting(true);
+    fetchJoin();
+    setIsSubmitting(false);
+  };
+  return (
+    <JoinModalWrapper>
+      <JoinModalContent>
+        <JoinModalTitle>Join us</JoinModalTitle>
+        <form onSubmit={handleSubmit(onSubmit)}>
+          <JoinModalInput
+            name="name"
+            placeholder="name"
+            value={userName}
+            onChange={(e: ChangeEvent<HTMLInputElement>) =>
+              setUserName(e.target.value)
+            }
+          />
+          <JoinModalInput
+            type="number"
+            name="country"
+            placeholder="country"
+            value={userNation}
+            onChange={(e: ChangeEvent<HTMLInputElement>) =>
+              setUserNation(Number(e.target.value))
+            }
+          />
+          <JoinModalAddress>{userAccount}</JoinModalAddress>
+          <JoinModalButton type="submit" disabled={isSubmitting}>
+            {isSubmitting ? "로딩..." : "회원가입"}
+          </JoinModalButton>
+        </form>
+      </JoinModalContent>
+    </JoinModalWrapper>
+  );
 };
 export default JoinModal;
 
 const colors = {
-    bg: {
-        main: "#101111",
-        footer: "#000",
-        box: "#1B1B1B",
-        icon: "#333",
-        iconHover: "#4B4B4B",
-        dotsActive: "#379FFF",
-    },
-    text: "#fff",
-    textGray: "#999",
-    footerText: "#A0A0A0",
-    lines: {
-        header: "#333",
-        footer: "#333",
-        slide: "#333",
-        navActive: "#d9d9d9",
-    },
+  bg: {
+    main: "#101111",
+    footer: "#000",
+    box: "#1B1B1B",
+    icon: "#333",
+    iconHover: "#4B4B4B",
+    dotsActive: "#379FFF",
+  },
+  text: "#fff",
+  textGray: "#999",
+  footerText: "#A0A0A0",
+  lines: {
+    header: "#333",
+    footer: "#333",
+    slide: "#333",
+    navActive: "#d9d9d9",
+  },
 };
 
-
 const JoinModalWrapper = styled.div`
-    position: fixed;
-    top: 0;
-    left: 0;
-    width: 100vw;
-    height: 100vh;
-    background-color: ${colors.bg.main};
-    z-index: 1000;
-    display: flex;
-    justify-content: center;
-    align-items: center;
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100vw;
+  height: 100vh;
+  background-color: ${colors.bg.main};
+  z-index: 1000;
+  display: flex;
+  justify-content: center;
+  align-items: center;
 `;
 
 const JoinModalContent = styled.div`
-    width: 500px;
-    padding: 20px;
-    background-color: ${colors.bg.box};
-    border-radius: 10px;
-    box-shadow: 0 0 10px rgba(0, 0, 0, 0.2);
-    color: ${colors.text};
+  width: 500px;
+  padding: 20px;
+  background-color: ${colors.bg.box};
+  border-radius: 10px;
+  box-shadow: 0 0 10px rgba(0, 0, 0, 0.2);
+  color: ${colors.text};
 `;
 
 const JoinModalTitle = styled.h1`
-    font-size: 1.8rem; // Adjust based on your fontSizes.title
-    font-weight: bold;
-    margin-bottom: 10px;
+  font-size: 1.8rem; // Adjust based on your fontSizes.title
+  font-weight: bold;
+  margin-bottom: 10px;
 `;
 
 const JoinModalInput = styled.input`
-    width: 100%;
-    padding: 10px;
-    border: 1px solid #ddd;
-    border-radius: 5px;
-    margin-bottom: 10px;
-    box-sizing: border-box;
+  width: 100%;
+  padding: 10px;
+  border: 1px solid #ddd;
+  border-radius: 5px;
+  margin-bottom: 10px;
+  box-sizing: border-box;
 `;
 
 const JoinModalAddress = styled.div`
-    width: 100%;
-    padding: 10px;
-    border: 1px solid #ddd;
-    border-radius: 5px;
-    margin-bottom: 10px;
-    box-sizing: border-box;
-    filter: grayscale(1);
+  width: 100%;
+  padding: 10px;
+  border: 1px solid #ddd;
+  border-radius: 5px;
+  margin-bottom: 10px;
+  box-sizing: border-box;
+  filter: grayscale(1);
 `;
 
 const JoinModalButton = styled.button`
-    width: 100%;
-    padding: 10px;
-    background-color: ${colors.bg.icon}; // Use a light gray color from your theme
-    color: ${colors.text}; // Use white text color
-    border: none;
-    border-radius: 5px;
-    cursor: pointer;
+  width: 100%;
+  padding: 10px;
+  background-color: ${colors.bg.icon}; // Use a light gray color from your theme
+  color: ${colors.text}; // Use white text color
+  border: none;
+  border-radius: 5px;
+  cursor: pointer;
 `;
 
 const JoinModalError = styled.div`
-    color: red;
-    margin-bottom: 10px;
+  color: red;
+  margin-bottom: 10px;
 `;
