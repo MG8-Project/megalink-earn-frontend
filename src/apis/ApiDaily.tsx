16831da340920fd7cf9a54242dcc04c2f2c286f3
primrose <gimmingyu0201@gmail.com>
2024-03-23 22:57:19 +0900

    feat: add myPoint

--- src/apis/ApiDaily.tsx
+++ src/apis/ApiDaily.tsx
@@ -4,34 +4,38 @@ const endpoint = process.env.REACT_APP_API_PERSONAL;
 const statusEndpoint = process.env.REACT_APP_API_STATUS;
 
 const myTotalLogin = async (userAccount: string) => {
-	return API
-	.post(`${endpoint}/myTotalLogin`, {userAccount: userAccount})
-	.then((res) => res.data)
-  .catch((error) => {
-    console.error("Fetching myTotalLogin failed: ", error);
-    throw error;
-  });
+    return API
+        .post(`${endpoint}/myTotalLogin`, {userAccount: userAccount})
+        .then((res) => res.data)
+        .catch((error) => {
+            console.error("Fetching myTotalLogin failed: ", error);
+            throw error;
+        });
 }
 const myParticipationTicket = async (userAccount: string) => {
-  return API.post(`${endpoint}/myParticipationTicket`, {
-    userAccount: userAccount,
-  })
-    .then((res) => res.data)
-    .catch((error) => {
-      console.error("Fetching myParticipationTicket failed: ", error);
-      throw error;
-    });
+    return API.post(`${endpoint}/myParticipationTicket`, {
+        userAccount: userAccount,
+    })
+        .then((res) => res.data)
+        .catch((error) => {
+            throw error;
+        });
 }
 const dailyPool = async () => {
-	return API
-	.get(`${statusEndpoint}/dailyPool`)
-	.then((res) => parseInt(res.data));
+    return API
+        .get(`${statusEndpoint}/dailyPool`)
+        .then((res) => parseInt(res.data));
+}
+
+const myPoint = async (userId: string) => {
+    return API.post(`${endpoint}/totalPoints`, {userId: userId})
 }
 
 const ApiDaily = {
-	myParticipationTicket,
-  myTotalLogin,
-  dailyPool,
+    myParticipationTicket,
+    myTotalLogin,
+    dailyPool,
+    myPoint,
 };
-  
+
 export default ApiDaily;
