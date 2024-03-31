ed8506ab0e0b2ee83dda193f06417ba36e9e42c2
augusstt06 <augusstt06@gmail.com>
2024-04-01 01:43:51 +0900

    feat: check airdrop is paused

--- src/component/Body/index.tsx
+++ src/component/Body/index.tsx
@@ -1,21 +1,44 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
 import styled from "styled-components";
 import Wallet from "../Wallet";
 import Status from "../Status";
 import Daily from "../Daily";
 import Spin from "../Spin";
 import LeaderBoard from "../LeaderBorad";
+import API from "../../apis/Api";
+
+interface Response {
+    status: string;
+    data: {
+        resultCode: string,
+        msg: string,
+        paused: boolean
+    }
+}
 
 const Body = () => {
-  return (
-    <BodyWrapper>
-      <Spin />
-      <Daily />
-      <LeaderBoard />
-      <Wallet />
-      <Status />
-    </BodyWrapper>
-  );
+    const [isAirDropPaused, setIsAirDropPaused] = useState(false);
+    useEffect(() => {
+        const fetchIsAirdropPaused = async () => {
+            try {
+                const API_ENDPOINT = `${process.env.REACT_APP_API_URL}/infiniteSpin/mega8/airdrop/isPaused`
+                const res: Response = await API.get(API_ENDPOINT)
+                setIsAirDropPaused(res.data.paused)
+            } catch (err) {
+                console.error(err)
+            }
+        }
+        void fetchIsAirdropPaused()
+    }, []);
+    return (
+        <BodyWrapper>
+            <Spin/>
+            <Daily/>
+            <LeaderBoard/>
+            {isAirDropPaused ? null : <Wallet/>}
+            <Status/>
+        </BodyWrapper>
+    );
 };
 
 export default Body;
