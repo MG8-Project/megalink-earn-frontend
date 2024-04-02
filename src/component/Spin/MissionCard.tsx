611119d49524495a150c09aecc2d666fa98f24ad
wjgin <woojin0913@naver.com>
2024-04-03 02:06:15 +0900

    fix: ticket charge time

--- src/component/Spin/MissionCard.tsx
+++ src/component/Spin/MissionCard.tsx
@@ -17,10 +17,23 @@ const MissionCard = () => {
     const nowUTC = Date.UTC(now.getUTCFullYear(), now.getUTCMonth(), now.getUTCDate(), now.getUTCHours(), now.getUTCMinutes(), now.getUTCSeconds());
 
     let nextChargeTime: Date;
-    if (now.getUTCHours() < 12) {
-      nextChargeTime = new Date(Date.UTC(now.getUTCFullYear(), now.getUTCMonth(), now.getUTCDate(), 12, 0, 0));
-    } else {
-      nextChargeTime = new Date(Date.UTC(now.getUTCFullYear(), now.getUTCMonth(), now.getUTCDate() + 1, 0, 0, 0));
+    // if (now.getUTCHours() < 12) {
+    //   nextChargeTime = new Date(Date.UTC(now.getUTCFullYear(), now.getUTCMonth(), now.getUTCDate(), 12, 0, 0));
+    // } else {
+    //   nextChargeTime = new Date(Date.UTC(now.getUTCFullYear(), now.getUTCMonth(), now.getUTCDate() + 1, 0, 0, 0));
+    // }
+    
+    // 현재 UTC 시간이 오전 7시 이전인 경우
+    if (now.getUTCHours() < 7) {
+      nextChargeTime = new Date(Date.UTC(now.getUTCFullYear(), now.getUTCMonth(), now.getUTCDate(), 7, 0, 0));
+    }
+    // 현재 UTC 시간이 오후 7시(19시) 이전인 경우
+    else if (now.getUTCHours() < 19) {
+      nextChargeTime = new Date(Date.UTC(now.getUTCFullYear(), now.getUTCMonth(), now.getUTCDate(), 19, 0, 0));
+    }
+    // 현재 UTC 시간이 오후 7시(19시) 이후인 경우, 다음 날 오전 7시를 nextChargeTime으로 설정
+    else {
+      nextChargeTime = new Date(Date.UTC(now.getUTCFullYear(), now.getUTCMonth(), now.getUTCDate() + 1, 7, 0, 0));
     }
 
     const difference = nextChargeTime.getTime() - nowUTC;
