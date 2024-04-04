7da617a5642ce2db05ef7e33da2592e50dac2416
yeonsu <rhdustn@imantisco.com>
2024-04-04 12:00:01 +0900

    feat : update WalletComponent

---
+++ src/component/Wallet/CoinCard.tsx
@@ -0,0 +1,113 @@
+import styled, { css } from "styled-components";
+import { theme } from "../../styles/theme";
+import { coinList } from "../../constants";
+import { ethers } from "ethers";
+import { useCallback, useEffect, useState } from "react";
+import { useAuthStore } from "../../store/authStore";
+
+interface CardBoxProps {
+  $highBalance: boolean;
+}
+
+const CoinCard = () => {
+  const userAddress = useAuthStore((state) => state.userAccount);
+  const [coins, setCoins] = useState(coinList);
+
+  const fetchBalances = useCallback(async () => {
+    if (!userAddress) {
+      return;
+    }
+
+    const updatedCoins = await Promise.all(
+      coins.map(async (item) => {
+        const provider = new ethers.JsonRpcProvider(item.url, item.chainId);
+        const balance = await provider.getBalance(userAddress);
+        const balanceInEther = ethers.formatEther(balance);
+        return { ...item, balance: balanceInEther };
+      })
+    );
+    setCoins(updatedCoins);
+  }, [userAddress, coins]);
+
+  useEffect(() => {
+    fetchBalances();
+  }, [fetchBalances]);
+
+  return (
+    <CardWrapper>
+      {coinList.map((item, index) => (
+        <CardBox key={index}>
+          <CardBoxImg src={item.image} alt="" />
+          <div>{item.title}</div>
+          <CardTextBox $highBalance={parseFloat(item.balance) < item.amount}>
+            <CardText $highBalance={parseFloat(item.balance) < item.amount}>
+              {item.balance}/{item.amount}
+            </CardText>
+          </CardTextBox>
+        </CardBox>
+      ))}
+    </CardWrapper>
+  );
+};
+
+export default CoinCard;
+
+const CardWrapper = styled.div`
+  display: flex;
+  gap: 24px;
+  margin-top: 60px;
+`;
+
+const CardBox = styled.div`
+  width: 384px;
+  height: 230px;
+  border-radius: 16px;
+  background-color: ${theme.colors.bg.box};
+  display: flex;
+  flex-direction: column;
+  gap: 20px;
+  align-items: center;
+`;
+
+//border gradients
+const CardTextBox = styled.div<CardBoxProps>`
+  background: linear-gradient(90deg, #82e8ff, #379fff);
+  border-radius: 100px;
+  border: 1px solid transparent;
+  background-image: linear-gradient(#000000, #000000),
+    linear-gradient(90deg, #82e8ff, #379fff);
+  background-origin: border-box;
+  background-clip: padding-box,
+    border-box
+      ${(props) =>
+        props.$highBalance &&
+        css`
+          border: 1px solid transparent;
+          background-image: linear-gradient(#000000, #000000),
+            linear-gradient(90deg, #333333, #333333);
+
+          background-origin: border-box;
+          background-clip: padding-box, border-box;
+        `};
+`;
+const CardText = styled.div<CardBoxProps>`
+  z-index: 100;
+  height: 40px;
+  border-radius: 20px;
+  background-color: black;
+  display: flex;
+  padding: 10px 24px;
+  justify-content: center;
+  align-items: center;
+  gap: 10px;
+  flex-direction: column;
+  gap: 20px;
+  justify-content: center;
+  align-items: center;
+  color: ${(props) => (props.$highBalance ? "#999999" : "inherit")};
+`;
+
+const CardBoxImg = styled.img`
+  width: 64px;
+  margin-top: 40px;
+`;
--- src/component/Wallet/index.tsx
+++ src/component/Wallet/index.tsx
@@ -1,83 +1,89 @@
 import styled from "styled-components";
 import PartnerToken from "./PartnerToken";
 import API from "../../apis/Api";
-import {useEffect, useState} from "react";
+import { useEffect, useState } from "react";
 import { useAuthStore } from "../../store/authStore";
+import CoinCard from "./CoinCard";
 
 export interface IToken {
-    "symbol": "string",
-    "logoUrl": "string",
-    "contractAddress": "string",
-    "chainId": "number",
-    "minAmount": "bigint",
-    "maxAmount": "bigint",
-    "decimals": "number"
+  symbol: "string";
+  logoUrl: "string";
+  contractAddress: "string";
+  chainId: "number";
+  minAmount: "bigint";
+  maxAmount: "bigint";
+  decimals: "number";
 }
 
 interface Response {
-    status: number;
-    data: {
-        "resultCode": "string",
-        "msg": "string",
-        "partnerTokens": IToken[]
-    }
-
+  status: number;
+  data: {
+    resultCode: "string";
+    msg: "string";
+    partnerTokens: IToken[];
+  };
 }
 
 interface ClaimAvailableResponse {
-    status: number;
-    data: {
-        resultCode: string,
-        msg: string,
-        claimable: boolean,
-        remainingTime: number
-    }
-
+  status: number;
+  data: {
+    resultCode: string;
+    msg: string;
+    claimable: boolean;
+    remainingTime: number;
+  };
 }
 
 const Wallet = () => {
-    const isLogin = useAuthStore((state) => state.isLoggedIn);
-    const [tokenList, setTokeList] = useState<IToken[]>([])
-    const [remainTime, setRemainTime] = useState<number>(0)
-    const [isClaimAvailable, setIsClaimAvailable] = useState<boolean>(false)
+  const isLogin = useAuthStore((state) => state.isLoggedIn);
+  const [tokenList, setTokeList] = useState<IToken[]>([]);
+  const [remainTime, setRemainTime] = useState<number>(0);
+  const [isClaimAvailable, setIsClaimAvailable] = useState<boolean>(false);
 
-    const fetchPartnerTokens = async () => {
-        try {
-            const API_ENDPOINT = `${process.env.REACT_APP_API_URL}/infiniteSpin/mega8/airdrop/partnerToken`
-            const res: Response = await API.get(API_ENDPOINT)
-            setTokeList(res.data.partnerTokens)
-        } catch (err) {
-            console.error(err)
-        }
+  const fetchPartnerTokens = async () => {
+    try {
+      const API_ENDPOINT = `${process.env.REACT_APP_API_URL}/infiniteSpin/mega8/airdrop/partnerToken`;
+      const res: Response = await API.get(API_ENDPOINT);
+      setTokeList(res.data.partnerTokens);
+    } catch (err) {
+      console.error(err);
     }
-    const fetchClaimAvailable = async () => {
-        try {
-            const API_ENDPOINT = `${process.env.REACT_APP_API_URL}/infiniteSpin/mega8/airdrop/claim/available `
-            const res: ClaimAvailableResponse = await API.get(API_ENDPOINT, {
-                headers: {
-                    Authorization: `Bearer ${localStorage.getItem('accessToken')}`
-                }
-            })
-            setIsClaimAvailable(res.data.claimable)
-            setRemainTime(res.data.remainingTime)
-        } catch (err) {
-            console.error(err)
-        }
+  };
+  const fetchClaimAvailable = async () => {
+    try {
+      const API_ENDPOINT = `${process.env.REACT_APP_API_URL}/infiniteSpin/mega8/airdrop/claim/available `;
+      const res: ClaimAvailableResponse = await API.get(API_ENDPOINT, {
+        headers: {
+          Authorization: `Bearer ${localStorage.getItem("accessToken")}`,
+        },
+      });
+      setIsClaimAvailable(res.data.claimable);
+      setRemainTime(res.data.remainingTime);
+    } catch (err) {
+      console.error(err);
     }
-    
-    useEffect(() => {
-        void fetchPartnerTokens();
-        void fetchClaimAvailable();
-    }, [isLogin, isClaimAvailable]);
+  };
 
-    return (
-        <WalletWrapper>
-            <WalletTitle>
-                Get ready for an exclusive airdrop event in 2 weeks!
-            </WalletTitle>
-            <PartnerToken tokenList={tokenList} remainTime={remainTime} isClaimAvailable={isClaimAvailable} setIsClaimAvailable={setIsClaimAvailable} isLogin={isLogin}/>
-        </WalletWrapper>
-    );
+  useEffect(() => {
+    void fetchPartnerTokens();
+    void fetchClaimAvailable();
+  }, [isLogin, isClaimAvailable]);
+
+  return (
+    <WalletWrapper>
+      <WalletTitle>
+        Get ready for an exclusive airdrop event in 2 weeks!
+      </WalletTitle>
+      <CoinCard />
+      {/* <PartnerToken
+        tokenList={tokenList}
+        remainTime={remainTime}
+        isClaimAvailable={isClaimAvailable}
+        setIsClaimAvailable={setIsClaimAvailable}
+        isLogin={isLogin}
+      /> */}
+    </WalletWrapper>
+  );
 };
 
 export default Wallet;
