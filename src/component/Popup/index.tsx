71a80070eedb304c31337db06327bdd7b063b21e
yeonsu <rhdustn@imantisco.com>
2024-04-04 12:08:40 +0900

    feat : popup update

--- src/component/Popup/index.tsx
+++ src/component/Popup/index.tsx
@@ -22,10 +22,7 @@ const Popup: React.FC<PopupProps> = (props) => {
   useEffect(() => {
     if (isOpen) {
       document.body.style.overflow = "hidden";
-    } else {
-      document.body.style.overflow = "visible";
     }
-
     return () => {
       document.body.style.overflow = "visible";
     };
